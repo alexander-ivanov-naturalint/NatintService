b28fbe4a6051be1dd1ce02145fb7e45af6a2b027
alexander-ivanov-naturalint <alexander.ivanov@naturalint.com>
2015-10-15 14:05:39 +0300

    JavaxMail.java now can download attachments and emails META_UA were removed from properties file

--- src/main/java/com/natint/email/JavaxMail.java
+++ src/main/java/com/natint/email/JavaxMail.java
@@ -1,10 +1,18 @@
 package com.natint.email;
 
 import com.natint.data.IData;
+import com.sun.mail.imap.IMAPFolder;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 
 import javax.mail.*;
 import javax.mail.internet.MimeBodyPart;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
@@ -14,41 +22,104 @@ import java.util.Properties;
 public class JavaxMail extends Email {
 
     private static final String EMAIL_PROTOCOL = System.getProperty("NATINT_EMAIL_PROTOCOL");
-    private static final String EMAIL_LOGIN = System.getProperty("NATINT_EMAIL_META_UA");
-    private static final String EMAIL_PASSWORD = System.getProperty("NATINT_PASSWORD_META_UA");
+    private static final String EMAIL_LOGIN = System.getProperty("NATINT_EMAIL_LOGIN");
+    private static final String EMAIL_PASSWORD = System.getProperty("NATINT_EMAIL_PASSWORD");
 
     @Override
     public List<IData> collectData() {
         try {
-            Folder folder = establishConnection();
-            Message[] messages = folder.getMessages();
+            Folder mailFolder = establishConnection();
+            File directory = prepareDirectory();
+            checkMailbox(mailFolder, directory);
 
-            for (Message message : messages) {
-                String contentType = message.getContentType();
-                if (contentType.contains("multipart")) {
-                    Multipart multipart = (Multipart) message.getContent();
-                    for (int i = 0; i < multipart.getCount(); i++) {
-                        MimeBodyPart bodyPart = (MimeBodyPart) multipart.getBodyPart(i);
-                        System.out.println("Body part content " + bodyPart.getContent());
-                    }
-                }
-            }
-        } catch (MessagingException | IOException e) {
+        } catch (MessagingException e) {
             e.printStackTrace();
         }
         return null;
     }
 
+    private File prepareDirectory()
+    {
+        DateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmssSSSSSSS");
+        Date dt = new Date();
+        String datetime = dateFormat.format(dt);
+
+        File dir = new File(datetime);
+        if (!dir.exists())
+            dir.mkdir();
+
+        return dir;
+    }
+
+    private List<String> checkMailbox(Folder folder, File outputDir)
+    {
+        try {
+            Message[] messages = folder.getMessages();
+
+            int msgIndex = 0;
+            for (Message message : messages)
+            {
+                Multipart multipart = (Multipart) message.getContent();
+                for (int i = 0; i < multipart.getCount(); i++)
+                {
+                    BodyPart bodyPart = multipart.getBodyPart(i);
+                    InputStream inputStream = null;
+                    String fileName = null;
+
+                    if (bodyPart.isMimeType("text/plain")  || bodyPart.isMimeType("text/html"))
+                    {
+                        String nameFormat = "%d-%s-%d.txt";
+                        fileName = String.format(nameFormat, msgIndex, "body", i);
+
+                        String body = (String)bodyPart.getContent();
+                        inputStream = new ByteArrayInputStream(body.getBytes(StandardCharsets.UTF_8));
+                    }
+                    else
+                    {
+                        if (!Part.ATTACHMENT.equalsIgnoreCase(bodyPart.getDisposition()) &&
+                                !StringUtils.isNotBlank(bodyPart.getFileName()))
+                        {
+                            continue; // dealing with attachments only
+                        }
+                        inputStream = bodyPart.getInputStream();
+                        String nameFormat = "%d-%s-%s";
+                        fileName = String.format(nameFormat, msgIndex, "att", bodyPart.getFileName());
+                    }
+
+                    if (inputStream != null && fileName != null) {
+                        File f = new File(outputDir.getAbsoluteFile() + "\\" + fileName);
+
+                        FileOutputStream fos = new FileOutputStream(f);
+                        byte[] buf = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.read(buf)) != -1) {
+                            fos.write(buf, 0, bytesRead);
+                        }
+                        fos.close();
+                    }
+                }
+
+                msgIndex++;
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     public Folder establishConnection() throws MessagingException {
         Properties properties = new Properties();
-        properties.setProperty("mail.store.protocol", "pop3");
+        //properties.setProperty("mail.store.protocol", "pop3");
 
-        Session session = Session.getDefaultInstance(properties);
+        properties.setProperty("mail.store.protocol", "imaps");
 
-        Store store = session.getStore();
+        Session session = Session.getDefaultInstance(properties, null);
+        Store store = session.getStore("imaps");
         store.connect(EMAIL_PROTOCOL, EMAIL_LOGIN, EMAIL_PASSWORD);
-
-        Folder folder = store.getFolder("INBOX");
+        IMAPFolder folder = (IMAPFolder) store.getFolder("Inbox");
         folder.open(Folder.READ_ONLY);
 
         return folder;
